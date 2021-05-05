71ffc7fff944d3c79fd1692a4421916333d95dbc
Austregecilio Cruz Neto <austregecilio@gmail.com>
2021-05-04 22:33:41 -0300

    Primeira versão, os filmes não estão alterando ao clicar no botão

---
+++ src/components/Content_old.tsx
@@ -0,0 +1,46 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import { api } from '../services/api';
+import { MovieCard } from './MovieCard';
+
+interface MovieProps {
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
+interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+
+export function Content() {
+  // Complete aqui
+  const [selectedGenre, setSelectedGenre] = useState<GenreResponseProps>({} as GenreResponseProps);
+  const [movies, setMovies] = useState<MovieProps[]>([]);
+
+  
+
+  return(
+    <>
+      <header>
+        <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
+      </header>
+
+      <main>
+        <div className="movies-list">
+          {movies.map(movie => (
+            <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+          ))}
+        </div>
+      </main>
+    </>
+  );
+}
